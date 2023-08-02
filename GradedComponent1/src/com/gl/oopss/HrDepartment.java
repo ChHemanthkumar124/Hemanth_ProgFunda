e3f6e30c838c03b77d41ebd117487bc87a80b6b9
HemanthKumar <141132307+ChHemanthkumar124@users.noreply.github.com>
2023-08-02 19:23:14 +0530

    Add files via upload

---
+++ GradedComponent1/src/com/gl/oopss/HrDepartment.java
@@ -0,0 +1,20 @@
+package com.gl.oopss;
+
+public class HrDepartment extends SuperDepartment {
+    
+    public String departmentName() {
+        return "Welcome to HR Department";
+    }
+    
+    public String getTodaysWork() {
+        return "Fill todays timesheet and mark your attendance";
+    }
+
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+
+    public String doActivity() {
+        return "Team Lunch";
+    }
+}
